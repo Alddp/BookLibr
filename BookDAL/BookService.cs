d4390f78e4cf8c4776d62538c98b4829a1180a1d
Alddp <1906701331@qq.com>
2025-05-31 20:31:04 +0800

    声明图书和借书服务函数
    在 `BookDAL.csproj` 中添加了 `OperService.cs` 和`BookService.cs`的编译引用。
    在 `BookService.cs` 中声明了图书信息插入、统计数量、模糊查询和更新库存的方法。
    在 `OperService.cs` 中声明了借书、续借、还书和查询借书记录的方法。

---
+++ BookDAL/BookService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BookDAL
+{
+    public class BookService
+    {
+        /// <summary>
+        /// TODO:新增图书信息（书名、作者、ISBN、封面等）
+        /// </summary>
+        /// <param name="bookName"></param>
+        /// <param name="author"></param>
+        /// <param name="ISBN"></param>
+        /// <param name="picture"></param>
+        /// <param name="price"></param>
+        /// <returns></returns>
+        public static int BookInsert(string bookName, string author, string ISBN, string picture, string price)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExNonQuery(sql, parameters);
+        }
+
+        /// <summary>
+        /// TODO:用于统计图书表数量
+        /// </summary>
+        /// <param name="bookId">图书编号</param>
+        /// <returns></returns>
+        public static int CountBookNum(string bookId)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExScalar(sql, parameters);
+        }
+
+        /// <summary>
+        /// TODO:模糊查询（书名、作者、ISBN）
+        /// </summary>
+        /// <param name="info">关键字（书名、作者、ISBN）</param>
+        /// <returns></returns>
+        public static SqlDataReader SearchBook(string info)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExecuteReader(sql, parameters);
+        }
+
+        // TODO:修改库存数量
+        public static int UpdateInventory(string bookId, int inventory)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExNonQuery(sql, parameters);
+        }
+    }
+}
---
+++ BookDAL/OperService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDAL
+{
+    public class OperService
+    {
+        // TODO: 借书
+        public static int BorrowBook(string userId, string bookId, string borrowAdminId, DateTime borrowDate)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExNonQuery(sql, parameters);
+        }
+
+        // TODO:续借操作
+        public static int RenewBook(string userId, string bookId, string renewAdminId, DateTime renewDate)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExNonQuery(sql, parameters);
+        }
+
+        // TODO: 还书
+        public static int ReturnBook(string userId, string bookId, string returnAdminId, DateTime returnDate)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExNonQuery(sql, parameters);
+        }
+
+        // TODO: 查询借书记录
+        public static SqlDataReader QueryBorrowRecord(string userId)
+        {
+            string sql = $"";
+            SqlParameter[] parameters = new SqlParameter[]
+               {
+               };
+            return DBHelper.ExecuteReader(sql, parameters);
+        }
+
+    }
+}
