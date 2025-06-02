7ce5c808b5796f150e78f27e7abea07a99789292
Alddp <1906701331@qq.com>
2025-06-02 14:25:03 +0800

    添加 ResultWrapper 类以封装操作结果
    在 `ResultWrapper.cs` 文件中，新增了 `ResultWrapper` 静态类及其 `Wrap<TData>` 方法，用于处理带返回值的操作，捕获并处理 SQL 异常和其他异常，返回相应的操作结果。
    
    在 `BookBLL.csproj` 文件中，添加了对 `Utils\ResultWrapper.cs` 文件的编译引用，使其参与项目编译。

---
+++ BookBLL/Utils/ResultWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookModels.Errors;
+using System.Data.SqlClient;
+
+namespace BookBLL.Utils {
+
+    public static class ResultWrapper {
+
+        /// <summary>
+        /// 封装带返回值的操作（如查询、登录等）
+        /// </summary>
+        /// <typeparam name="TData"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static OperationResult<TData, ErrorCode> Wrap<TData>(Func<TData> func) {
+            try {
+                TData result = func();
+                return OperationResult<TData, ErrorCode>.Ok(result);
+            }
+            catch (SqlException ex) {
+                if (ErrorMessages.IsConflictError(ex))
+                    return OperationResult<TData, ErrorCode>.Fail(ErrorCode.DuplicateKey, "数据重复：" + ex.Message);
+                return OperationResult<TData, ErrorCode>.Fail(ErrorCode.DatabaseError, "数据库异常：" + ex.Message);
+            }
+            catch (Exception ex) {
+                return OperationResult<TData, ErrorCode>.Fail(ErrorCode._error, "未知异常：" + ex.Message);
+            }
+        }
+    }
+}
